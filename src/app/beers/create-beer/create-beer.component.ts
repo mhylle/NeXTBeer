1c166c93806019130e4a17c194a8acb1abb97ef1
Martin Hylleberg <mhylle@yahoo.com>
2017-11-16 22:55:14 +0100

    it is now possible to create a beer from the create beer component

--- src/app/beers/create-beer/create-beer.component.ts
+++ src/app/beers/create-beer/create-beer.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {Beer} from "../Beer";
+import {BeerService} from "../services/beer.service";
 
 @Component({
-  selector: 'app-create-beer',
+  selector: 'create-beer',
   templateUrl: './create-beer.component.html',
   styleUrls: ['./create-beer.component.css']
 })
 export class CreateBeerComponent implements OnInit {
 
-  constructor() { }
+  newBeer: Beer = new Beer();
+  constructor(private beerService: BeerService) { }
 
   ngOnInit() {
   }
 
+  createBeer() {
+    this.beerService.addBeer(this.newBeer);
+    this.newBeer = new Beer();
+  }
 }
