c64c9dd19aeb50a000ced6a496739bafaa6afe2c
Martin Hylleberg <mhylle@yahoo.com>
2018-02-05 10:19:13 +0100

    Update beer.service.ts

--- src/app/beers/services/beer.service.ts
+++ src/app/beers/services/beer.service.ts
@@ -9,24 +9,8 @@ export class BeerService {
 
   beers: Beer[] = [];
 
-  constructor(private db: AngularFireDatabase) {
-    let b1 = new Beer();
-    b1.id = 1;
-    b1.name = 'Londons Pride';
-    b1.description = 'Very good beer with just the right balances in the ingredients';
-    b1.type = 'Premium Ale';
-    b1.brewery = 'Fullers';
-    this.beers.push(b1);
-    let b2 = new Beer();
-    b2.id = 2;
-    b2.name = 'Warsteiner';
-    b2.description = 'Generic pilsner';
-    b2.type = 'Pilsner';
-    b2.brewery = 'Warsteiner';
-    this.beers.push(b2);
-
+  constructor(private db: AngularFireDatabase) {   
     this.beerObservable = this.getBeers();
-
   }
 
   getBeers(): Observable<Beer[]> {
