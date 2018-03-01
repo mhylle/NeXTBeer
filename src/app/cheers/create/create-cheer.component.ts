d4b02825b134d269b5d7687d656688accb9f6f0f
Martin Hylleberg <mhylle@yahoo.com>
2018-03-01 09:17:13 +0100

    added predefined cheers

--- src/app/cheers/create/create-cheer.component.ts
+++ src/app/cheers/create/create-cheer.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {CheerService} from "../services/cheer.service";
 import {Cheer} from "../Cheer";
+import {UserService} from "../../users/services/user.service";
 
 
 @Component({
@@ -15,15 +16,47 @@ export class CreateCheerComponent implements OnInit {
   @Output()
   quoteCreated: EventEmitter<Cheer> = new EventEmitter();
 
-  constructor(private cheerService: CheerService) {
+  predefinedCheers: Cheer[] =[];
+  constructor(private cheerService: CheerService, private userService: UserService) {
   }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(value => {
+      if (value != null) {
+        for (let i = 0; i < value.length; i++) {
+          let user = value[i];
+          let c1 = new Cheer();
+          c1.cheer = "Skål";
+          c1.author = user.firstname + " " + user.familyname;
+          this.predefinedCheers.push(c1)
+          let c2 = new Cheer();
+          c2.cheer = "Det er guf det her!";
+          c2.author = user.firstname + " " + user.familyname;
+          this.predefinedCheers.push(c2)
+        }
+      }
+    });
   }
 
-  createCheer() {
+  // createCheer() {
+  //   let now = new Date();
+  //   this.newCheer.creationTime = {
+  //     day: now.getDate(),
+  //     month: now.getMonth() + 1,
+  //     year: now.getFullYear(),
+  //     hour: now.getHours(),
+  //     minute: now.getMinutes(),
+  //     second: now.getSeconds()
+  //
+  //   };
+  //   this.cheerService.addCheer(this.newCheer);
+  // }
+  createCheer(cheer: Cheer) {
+    if (cheer == null) {
+      cheer = this.newCheer;
+    }
     let now = new Date();
-    this.newCheer.creationTime = {
+    cheer.creationTime = {
       day: now.getDate(),
       month: now.getMonth() + 1,
       year: now.getFullYear(),
@@ -32,7 +65,7 @@ export class CreateCheerComponent implements OnInit {
       second: now.getSeconds()
 
     };
-    this.cheerService.addCheer(this.newCheer);
+    this.cheerService.addCheer(cheer);
   }
 
 }
